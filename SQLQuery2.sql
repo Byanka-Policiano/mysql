4cdb474038abbb0de3606987988b9e07a93ca169
Byanka Policiano Santos <policianobyanka@gmail.com>
2023-09-14 11:43:00 -0300

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,3 +1,4 @@
+
 -- Criando o banco de dados
 create database reserva_de_hotel;
 -- Usando o sistema biblioteca
@@ -21,6 +22,10 @@ create table tbl_evento(
 	num_participantes varchar(400),
 	tipo varchar(15)
 );
+--Alterar o tipo da coluna
+ALTER TABLE tbl_evento 
+ALTER COLUMN num_participantes int;
+
 create table tbl_quarto_suite(
 	id_quarto_suite int primary key,
 	capacidade varchar(4),
@@ -28,6 +33,13 @@ create table tbl_quarto_suite(
 	comodidades varchar(150),
 	cama char(1)
 );
+
+ALTER TABLE tbl_quarto_suite
+ALTER COLUMN capacidade int;
+
+ALTER TABLE tbl_quarto_suite
+ALTER COLUMN cama varchar(15);
+
 create table tbl_endereco(
 	id_endereco int primary key,
 	rua varchar(10),
@@ -51,18 +63,51 @@ create table tbl_reserva(
 	foreign key (id_evento) references tbl_evento(id_evento)
 );
 
-create table tbl_manutenção(
+create table tbl_manutencao(
 	id_funcionario int,
 	id_quarto_suite int,
 	foreign key (id_funcionario) references tbl_funcionario(id_funcionario),
 	foreign key (id_quarto_suite) references tbl_quarto_suite(id_quarto_suite)
 );
+-- Alterar o nome da tabela
+EXEC sp_rename 'tbl_manutenção', 'tbl_manutencao';
 
 
-
-create table tbl_organização(
+create table tbl_organizacao(
 	id_funcionario int,
 	id_evento int,
 	foreign key (id_funcionario) references tbl_funcionario(id_funcionario),
 	foreign key (id_evento) references tbl_evento(id_evento)
 );
+
+EXEC sp_rename 'tbl_organização', 'tbl_organizacao';
+
+-- Inserindo os dados na tabela
+insert into tbl_cliente(id_cliente, nome, contato)
+values (2, 'José', 5581988838195);
+--selecionar os dados de uma tabela
+select * from tbl_cliente
+
+insert into tbl_endereco(id_endereco, rua, numero, uf, cep, bairro, cidade)
+values (5, 'Jerusalém', '12', 'RN', '59088107', 'Neópolis', 'Natal')
+select * from tbl_endereco
+
+insert into tbl_evento(id_evento, nome, dia, horario, num_participantes, tipo)
+values(3, 'The Weeknd', '2023-12-15', '20:30:00', 12, 'Show')
+select * from tbl_evento
+
+insert into tbl_funcionario(id_funcionario, nome, cargo)
+values (2, 'Arlindo', 'zelador')
+select * from tbl_funcionario
+
+insert into tbl_quarto_suite(id_quarto_suite, capacidade, preco_noite, comodidades, cama)
+values (105, 4, 249.99, 'ar-condicionado, televisão, frigobar', 'Queen')
+select * from tbl_quarto_suite
+
+insert into tbl_manutencao(id_funcionario, id_quarto_suite)
+values (14, 105)
+select * from tbl_manutencao
+ 
+insert into tbl_reserva(cod_evento, cod_quarto_suite, id_cliente, id_evento, id_funcionario, id_quarto_suite)
+values (5, 12, 2, 3, 14, 105)
+select * from tbl_reserva
